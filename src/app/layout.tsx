6fc1a7ff95f123590899619feae4aca952875b4c
pengineer-geek <pengineer.geek@gmail.com>
2025-08-17 00:06:14 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,72 +1,24 @@
-import Footer from "@/app/_components/footer";
-import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
+// src/app/layout.tsx（抜粋）
+import "./globals.css";
 import type { Metadata } from "next";
+import Header from "./_components/header";
+import Footer from "./_components/footer";
 import { Inter } from "next/font/google";
 import cn from "classnames";
-import { ThemeSwitcher } from "./_components/theme-switcher";
-
-import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://pengineer-geek-blog.vercel.app"),
-  title: `ペンジニアの技育(ギーク)ブログ`,
-  description: `IT業界の荒波に揉まれるうちにペンギンになってしまったエンジニア.`,
-  images: [
-      {
-        url: "/banner-yokohama.png",
-        width: 1200,
-        height: 630,
-        alt: "ペンジニアの技育（ギーク）ブログ",
-      },
+  title: "ペンジニアの技育（ギーク）ブログ",
+  description: "IT業界の荒波に揉まれるうちにペンギンになってしまったエンジニア。",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
-          color="#000000"
-        />
-        <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <meta name="msapplication-TileColor" content="#000000" />
-        <meta
-          name="msapplication-config"
-          content="/favicon/browserconfig.xml"
-        />
-        <meta name="theme-color" content="#000" />
-        <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      </head>
-      <body
-        className={cn(inter.className, "dark:bg-slate-900 dark:text-slate-400")}
-      >
-        <ThemeSwitcher />
-        <div className="min-h-screen">{children}</div>
+    <html lang="ja">
+      <body className={cn(inter.className, "bg-background text-text")}>
+        <Header />
+        {children}
         <Footer />
       </body>
     </html>
