b975061f7f49e7611fe28e467921314066938e29
pengineer <pengineer.geek@gmail.com>
2025-08-29 01:48:16 +0900

    icon追加

--- src/app/_components/icons/index.tsx
+++ src/app/_components/icons/index.tsx
@@ -276,9 +276,17 @@ export function IconX(props: React.SVGProps<SVGSVGElement>) {
 // 検索
 export function IconSearch(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search-icon lucide-search" {...props}>
-      <path d="m21 21-4.34-4.34"/>
-      <circle cx="11" cy="11" r="8"/>
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="m21 21-4.34-4.34" />
+      <circle cx="11" cy="11" r="8" />
     </svg>
   );
 }
